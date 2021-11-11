881e056abded04684f93e5f75e6710c9e097aeeb
HwToLrn <pksmb3030@gmail.com>
2021-11-11 12:11:11 +0900

    network shape 파악
    y, feature = net(x)의 값을 알아본다

--- Examples/explore_easyocr_vgg16bn.py
+++ Examples/explore_easyocr_vgg16bn.py
@@ -5,16 +5,31 @@ import cv2
 import torch.nn.functional as F
 
 
-def test_net(canvas_size, mag_ratio, net, image, text_threshold, link_threshold, low_text, poly, device, estimate_num_chars=False):
-    pass
+class double_conv(torch.nn.Module):
+    def __init__(self, in_ch, mid_ch, out_ch):
+        super(double_conv, self).__init__()
+        self.conv = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels=in_ch + mid_ch, out_channels=mid_ch, kernel_size=(1, 1)),
+            torch.nn.BatchNorm2d(mid_ch),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Conv2d(in_channels=mid_ch, out_channels=out_ch, kernel_size=(3, 3), padding=1),
+            torch.nn.BatchNorm2d(out_ch),
+            torch.nn.ReLU(inplace=True)
+        )
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.conv.to(self.device)
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
 
 
 # easyocr.craft.CRAFT() code comprehension
 def main():
     # data
-    image = '../ImageDatas/image1.jpg'
+    image = '../ImageDatas/image1.jpg'  # shape : (243, 208, 3)
     image, _ = easyocr.utils.reformat_input(image=image)
-    print('image.shape : ', image.shape)
+    print('image.shape : ', image.shape)  # -> height, width, channel
     print('type(image) : ', type(image))
 
     # hyper parameters
@@ -66,15 +81,51 @@ def main():
     #     print(source[idx])
     # print()
 
+    # U network
+    upconv1 = double_conv(1024, 512, 256)
+    upconv2 = double_conv(512, 256, 128)
+    upconv3 = double_conv(256, 128, 64)
+    upconv4 = double_conv(128, 64, 32)
+
     # torch.cat() : https://sanghyu.tistory.com/85
     print(source[0].shape, '\tsource[0].shape')  # torch.Size([1, 1024, 14, 16])
     print(source[1].shape, '\tsource[1].shape')  # torch.Size([1, 512, 14, 16])
-    fst_unet = torch.cat(tensors=[source[0], source[1]], dim=1)
-    print(fst_unet.shape, '\t0, 1 concatenated shape')  # torch.Size([1, 1536, 14, 16])
+    fst_unet = torch.cat(tensors=[source[0], source[1]], dim=1)  # dim=1: 2번째 차원
+    fst_unet_out = upconv1(fst_unet)
+    print(fst_unet.shape, '\tfst_unet shape')  # torch.Size([1, 1536, 14, 16])
+    print(fst_unet_out.size(), '\tfst_unet_out.size()')
     print('='*50)
-    print(source[2].size(), '\tsource[2].size()')  # -> torch.Size([1, 512, 28, 32])
+
     # fst_unet과 source[2]의 shape을 맞추기 위해 F.interpolate()를 사용한다.
-    scd_unet = F.interpolate(input=fst_unet, size=sources[2].size()[2:], mode='bilinear', align_corners=False)
+    print(source[2].size(), '\tsource[2].size()')  # torch.Size([1, 512, 28, 32])
+    y = F.interpolate(input=fst_unet_out, size=source[2].size()[2:], mode='bilinear', align_corners=False)
+    scd_unet = torch.cat(tensors=[y, source[2]], dim=1)
+    scd_unet_out = upconv2(scd_unet)
+    print(y.size(), '\ty.size()')
+    print(scd_unet.size(), '\tscd_unet.size()')
+    print(scd_unet_out.size(), '\tscd_unet_out.size()')
+    print('='*50)
+
+    print(source[3].size(), '\tsource[3].size()')  # torch.Size([1, 512, 28, 32])
+    y = F.interpolate(input=scd_unet_out, size=source[3].size()[2:], mode='bilinear', align_corners=False)
+    trd_unet = torch.cat(tensors=[y, source[3]], dim=1)
+    trd_unet_out = upconv3(trd_unet)
+    print(y.size(), '\ty.size()')
+    print(trd_unet.size(), '\ttrd_unet.size()')
+    print(trd_unet_out.size(), '\ttrd_unet_out.size()')
+    print('='*50)
+
+    print(source[4].size(), '\tsource[4].size()')  # torch.Size([1, 512, 28, 32])
+    y = F.interpolate(input=trd_unet_out, size=source[4].size()[2:], mode='bilinear', align_corners=False)
+    y_out = torch.cat(tensors=[y, source[4]], dim=1)
+    feature = upconv4(y_out)
+    print(y.size(), '\ty.size()')
+    print(y_out.size(), '\ty_out.size()')
+    print(feature.size(), '\tfeature.size()')
+    print('='*50)
+
+    y_out = y_out.permute(0, 2, 3, 1)  # shape : data num, height, width, channel
+    print(y_out.size(), '\ty_out.size()')
 
 
 if __name__ == '__main__':
